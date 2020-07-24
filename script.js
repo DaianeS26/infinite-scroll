206f8d6fb01c4507a6b65be0a8e49f6444963c79
Daianes26 <daianeasilva26@gmail.com>
2020-07-23 22:17:44 -0400

    Improved implementation

--- script.js
+++ script.js
@@ -7,23 +7,32 @@ const count= 10;
 const apiKey = 'k8fa36h4Y3aJBuBKu6L9sGAggfbYHfjnp40JLINbu94';
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
+function setAttributes(element, attributes) {
+    for (const key in attributes) {
+        element.setAttribute(key, attributes[key])
+    }
+}
+
 function displayPhotosElement() {
     photosArray.forEach((photo) => {
         //Create <a> to link to Unsplash
         const item = document.createElement('a');
-        item.setAttribute('href', photo.links.html);
-        item.setAttribute('target', '_blank');
         //Create <img> for photo
+        setAttributes(item, {
+            href: photo.links.html,
+            target: '_blank',
+        });
         const img = document.createElement('img');
-        img.setAttribute('src', photo.urls.regular);
-        img.setAttribute('alt', photo.alt_description);
-        img.setAttribute('title', photo.alt_description);
         //Put <img> inside <a>, then put both inside image container element
+        setAttributes(img, {
+           src: photo.urls.regular,
+           alt: photo.alt_description,
+           title: photo.alt_description
+        });
         item.appendChild(img);
         imageContainer.appendChild(item);
     });
 }
-
 
 async function getPhotosAPI() {
     try {
